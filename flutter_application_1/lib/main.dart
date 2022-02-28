6a75d3cbb71a1c27b01a9de90f1fc697d5db385e
Nutthaweed <97228092+Nutthaweed@users.noreply.github.com>
2022-02-28 23:36:32 +0700

    first commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Dice App",
+      home: HomePage(),
+    ));
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int diceNumber = 1;
+
+  changeDice() {
+    setState(() {
+      diceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      appBar: AppBar(
+        title: const Text("Dice Roller"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Column(
+        children: [
+          const SizedBox(height: 20),
+          // Image
+          Center(
+              child: Image.asset(
+            "assets/$diceNumber.png",
+            height: 250,
+            width: 250,
+          )),
+          // Button
+          MaterialButton(
+            color: Colors.green,
+            onPressed: changeDice,
+            child: const Text(
+              "Roll",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          const SizedBox(height: 70),
+          // Text
+          const Text("Created by Nutthaweed",
+              style: TextStyle(fontWeight: FontWeight.bold)),
+        ],
+      ),
+    );
+  }
+}
